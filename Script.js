ff6cb969a1737954a7f0f5e1ec9e3725cdac4ba0
Yashwanthdeveloper <128028941+Yashwanthdeveloper@users.noreply.github.com>
2023-09-27 22:45:13 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -18,3 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
                     
+
+
+
+
+// JavaScript for changing background color every 5 seconds
+const colors = ['#f0f0f0', '#d3d3d3', '#c0c0c0', '#a9a9a9']; // List of background colors
+let currentIndex = 0; // Start with the first color
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = colors[currentIndex]; // Change the background color
+    currentIndex = (currentIndex + 1) % colors.length; // Move to the next color
+}
+
+// Call the function to change the background color every 5 seconds
+setInterval(changeBackgroundColor, 5000);
