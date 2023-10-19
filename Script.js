f34ec14eea358c8035d3416aa380a0057d787ff4
Yashwanthdeveloper <128028941+Yashwanthdeveloper@users.noreply.github.com>
2023-10-19 22:47:30 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -22,6 +22,85 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+// Get the container element
+var container = document.getElementById("iframeContainer");
+
+// Function to read links from links.txt
+function readLinksFromFile() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "links.txt", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var links = xhr.responseText.split("\n"); // Assuming each link is on a new line
+            createIframes(links);
+        }
+    };
+    xhr.send();
+}
+
+// Function to create and add iframes with buttons
+function createIframes(links) {
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i].trim();
+        if (link !== "") {
+            var iframe = document.createElement("iframe");
+            iframe.src = link;
+            iframe.className = "iframe"; // Apply the "iframe" class for styling
+
+            // Create a button to open the link in a new tab
+            var openButton = document.createElement("button");
+            openButton.innerText = "Open";
+
+            // Use a function to capture the correct link for the button
+            openButton.addEventListener("click", (function (url) {
+                return function () {
+                    window.open(url, "_blank");
+                };
+            })(link));
+
+            // Create a container div for the iframe and button
+            var containerDiv = document.createElement("div");
+            containerDiv.appendChild(iframe);
+            containerDiv.appendChild(openButton);
+
+            container.appendChild(containerDiv);
+        }
+    }
+}
+
+// Call the function to read links from the file and create iframes with buttons
+readLinksFromFile();
+                          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // JavaScript for changing background color every 5 seconds
 const colors =["#000080", "#000000", "#191970", "#C0C0C0", "#4169E1"]
 let currentIndex = 0; // Start with the first color
@@ -33,3 +112,4 @@ function changeBackgroundColor() {
 
 // Call the function to change the background color every 5 seconds
 setInterval(changeBackgroundColor, 5000);
+*/
